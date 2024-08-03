c743a32c333891239d49720ad0bbd07ecbe93213
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-03 15:33:22 +0500

    create json data for music course

--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-function Contact(){
-    return<>
-    <div className="max-w-7xl mx-auto">
-      <h1 className="text-center text-3xl">This is contact page</h1>
-    </div>
-    </>
-}
-export default Contact
