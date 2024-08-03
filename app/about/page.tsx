b688fc99cae6f164efaed81e5bc48bd84d54a9ce
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-03 12:49:51 +0500

    create about page and change metadata

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+function About(){
+    return<>
+   <div className="max-w-7xl mx-auto">
+      <h1 className="text-center text-3xl">This is about page</h1>
+    </div>
+    </>
+}
+export default About
