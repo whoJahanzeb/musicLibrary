9d79f22a60ea944b6fa9a8ec9daf40b81e7be584
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-04 13:25:24 +0500

    create animated tooltip section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import FeaturedCourses from "./components/FeaturedCourses";
 import WhyChooseUs from "./components/WhyChooseUs";
 import TestimonialCards from "./components/TestimonialCards";
 import UpcomingWebinars from "./components/UpcomingWebinars";
+import AnimatedTooltipPreview from "./components/AnimatedTooltipPreview";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCards />
       <UpcomingWebinars />
+      <AnimatedTooltipPreview />
     </main>
   );
 }
