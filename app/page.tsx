8d7d594a82291a4cda1bd13e0586b9fd54a3a439
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-04 12:29:28 +0500

    create UI/UX for testiMonial section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import FeaturedCourses from "./components/FeaturedCourses";
 import WhyChooseUs from "./components/WhyChooseUs";
+import TestimonialCards from "./components/TestimonialCards";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <TestimonialCards />
     </main>
   );
 }
