96ade98ab8d8b08df8daf67f749d918da6b20161
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-06 23:09:13 +0500

    set responsivness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar({ className }: { className?: string }) {
     <>
       <div
         className={cn(
-          "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50",
+          "relative md:fixed md:top-10 inset-x-0 max-w-lg md:max-w-2xl mx-auto z-50",
           className
         )}
       >
